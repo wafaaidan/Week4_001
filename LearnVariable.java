48bfa05e3bc208c5e2fb7cabe2c3e0802d7ccc80
wafaaidan <wafa.aidan@gmail.com>
2025-10-21 15:44:26 +0700

    Add Scanner umur

--- LearnVariable.java
+++ LearnVariable.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class LearnVariable{
     public static void main(String[] args) {
+        Scanner newScan = new Scanner(System.in);
         int umur;
         String nama;
+        System.out.println("masukkan umur= ");
+        umur= newScan.nextInt();
     }
 }
